21e68081fb0133b5eec7b06c5ac58955e94588c2
Kaustubh Khilari <kvstubh@gmail.com>
2025-01-06 22:20:56 +0300

    Added Carousel and Different Channels with Videos for Viewers on Hover

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,38 @@
 // Home.js
 import styled from 'styled-components';
 import Header from './Header';
+import ImgSlider from './imgSlider';
+import Viewers from './Viewers';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Container>
+				<ImgSlider />
+				<Viewers />
+			</Container>
 		</>
 	);
 };
+
+const Container = styled.main`
+	position: relative;
+	min-height: calc(100vh - 250px);
+	overflow-x: hidden;
+	display: block;
+	top: 72px;
+	padding: 0 calc(3.5vw + 5px);
+
+	&:after {
+		background: url('/images/home-background.png') center center / cover
+			no-repeat fixed;
+		content: '';
+		position: absolute;
+		inset: 0px;
+		opacity: 1;
+		z-index: -1;
+	}
+`;
 
 export default Home;
